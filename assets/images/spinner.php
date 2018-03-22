7c405df4269d9b450ebcf67398ae6b66c04f7d3b
Richard Tabor <hello@themebeans.com>
2018-03-22 13:23:02 -0400

    👌 IMPROVE: Clean up the loading button spinner

--- assets/images/spinner.php
+++ assets/images/spinner.php
@@ -1,48 +1,11 @@
 <span class="merlin__button--loading__spinner">
-<cite class="merlin-spinner"><!--
---><cite class="merlin-spinner-line merlin-spinner-line--1"><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--left"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-ticker"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--center"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--right"></cite><!--
---></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line merlin-spinner-line--2"><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--left"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-ticker"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--center"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--right"></cite><!--
---></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line merlin-spinner-line--3"><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--left"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-ticker"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--center"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--right"></cite><!--
---></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line merlin-spinner-line--4"><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--left"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-ticker"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--center"></cite><!--
---></cite><!--
---><cite class="merlin-spinner-line-cog"><!--
---><cite class="merlin-spinner-line-cog-inner merlin-spinner-line-cog-inner--right"></cite><!--
---></cite><!--
---></cite><!--
---></cite><!--/spinner -->
+
+	<div class="merlin-spinner">
+
+		<svg class="merlin-spinner__svg" viewbox="25 25 50 50">
+			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="6" stroke-miterlimit="10"></circle>
+		</svg>
+
+	</div>
+
 </span>
